ba0c320af38c145359dcd5f5f3cecf505a2b725a
Joel Butler <joel.e.butler04@gmail.com>
2023-06-04 20:42:42 -0400

    Added LOCOMOS support
    Added LOCOMOS support for #26

---
+++ src/ewx_pws/locomos.py
@@ -0,0 +1,116 @@
+import time, json
+from requests import Session, Request
+from datetime import datetime, timezone
+
+from ewx_pws.weather_stations import WeatherStationConfig, WeatherStationReading, WeatherStationReadings, WeatherStation, STATION_TYPE
+
+class LocomosConfig(WeatherStationConfig):
+        station_id     : str
+        station_type   : STATION_TYPE = 'LOCOMOS'
+        token          : str # Device token
+        id             : str # ID field on device webpage
+        tz             : str = 'ET'
+
+class LocomosStation(WeatherStation):
+    """ config is RainwiseConfig type """
+    @classmethod
+    def init_from_dict(cls, config:dict):
+        """ accept a dictionary to create this class, rather than the Type class"""
+
+        # this will raise error if config dictionary is not correct
+        station_config = LocomosConfig.parse_obj(config)
+        return(cls(station_config))
+
+    def __init__(self,config: LocomosConfig):
+        """ create class from config Type"""
+        super().__init__(config)
+
+    def _check_config(self):
+        # TODO implement 
+        return(True)
+
+    def _get_readings(self, start_datetime:datetime, end_datetime:datetime):
+        """
+        Params are start time, end time.
+        Returns api response in a list with metadata
+        """
+
+        start_milliseconds=int(time.mktime(start_datetime.timetuple())) * 1000
+        end_milliseconds=int(time.mktime(end_datetime.timetuple())) * 1000
+
+        var_request = Request(method='GET',
+                url='https://industrial.api.ubidots.com/api/v2.0/devices/{}/variables/'.format(self.config.id), 
+                headers={'X-Auth-Token': self.config.token}, 
+                params={'page_size':'ALL'}).prepare()
+        var_response = json.loads(Session().send(var_request).content)
+        var_list = {}
+        for result in var_response['results']:
+            var_list[result['label']] = result['id']
+        
+        data = {}
+        for var in var_list:
+            request = Request(method='GET',
+                url='https://industrial.api.ubidots.com/api/v1.6/variables/{}/values'.format(var_list[var]), 
+                headers={'X-Auth-Token':self.config.token}, 
+                params={'page_size':'ALL', 'start':int(start_milliseconds), 'end':int(end_milliseconds)}).prepare()
+            response = json.loads(Session().send(request).content)
+            data[var] = response
+        return_dict = {
+            'station_id': self.config.station_id,
+            'request_datetime': datetime.utcnow(),
+            'data': data
+        }
+        return [return_dict]
+
+
+    def _transform(self, data=None):
+        """
+        Transforms data into a standardized format and returns it as a WeatherStationReadings object.
+        data param if left to default tries for self.response_data processing
+        """
+        readings_list = WeatherStationReadings()
+
+        results = data['data']
+        if 'precip' not in results.keys() or 'humidity' not in results.keys() or 'temperature' not in results.keys():
+            return readings_list
+        timestamps = []
+        for key in ['precip', 'humidity', 'temperature']:
+            for entry in results[key]['results']:
+                if entry['timestamp'] not in timestamps:
+                    timestamps.append(entry['timestamp'])
+        timestamps.sort()
+        for timestamp in timestamps:
+            temp = None
+            precip = None
+            humidity = None
+            for result in results['precip']['results']:
+                if result['timestamp'] == timestamp:
+                    precip = result['value']
+            for result in results['temperature']['results']:
+                if result['timestamp'] == timestamp:
+                    temp = result['value']
+            for result in results['humidity']['results']:
+                if result['timestamp'] == timestamp:
+                    humidity = result['value']
+
+            temp = LocomosReading(station_id=data['station_id'],
+                                    request_datetime=datetime.utcnow(),
+                                    data_datetime=datetime.fromtimestamp(timestamp / 1000),
+                                    atemp=temp,
+                                    pcpn=round(precip * 25.4, 2),
+                                    relh=humidity)
+            readings_list.readings.append(temp)
+            
+        return readings_list
+
+    def _handle_error(self):
+        """ place holder to remind that we need to add err handling to each class"""
+        pass
+
+class LocomosReading(WeatherStationReading):
+    station_id : str
+    request_datetime : datetime or None = None # UTC
+    data_datetime : datetime           # UTC
+    atemp : float or None = None       # celsius 
+    pcpn : float or None = None        # mm, > 0
+    relh : float or None = None        # percent
---
+++ tests/test_locomos_stations.py
@@ -0,0 +1,63 @@
+from ewx_pws.locomos import LocomosConfig, LocomosStation, WeatherStationConfig, WeatherStation
+import pytest, logging
+from datetime import datetime
+
+# note: fixtures auto-imported from conftest.py
+
+@pytest.fixture
+def station_type():
+    return 'LOCOMOS'
+
+@pytest.fixture
+def test_station(station_configs, station_type):
+    station = LocomosStation.init_from_dict(station_configs[station_type])
+    return(station)
+    
+def test_locomos_config(fake_station_configs,station_type):
+    c = LocomosConfig.parse_obj(fake_station_configs[station_type])
+    assert isinstance(c, LocomosConfig)
+    
+def test_locomos_class_fake(fake_station_configs,station_type):
+    station = LocomosStation.init_from_dict(fake_station_configs[station_type])
+    assert isinstance(station, WeatherStation)
+    assert isinstance(station.id, str)
+    
+def test_locomos_class_instantiation_from_config(station_configs,station_type):
+    config = station_configs[station_type]
+    logging.debug('config used: {}'.format(config))
+    station = LocomosStation.init_from_dict(config)
+    assert isinstance(station, WeatherStation)
+    assert isinstance(station.id, str)
+    
+def test_locomos_readings(test_station):
+    
+    # note as of 2023-03-26 the test best Locomos station is offline
+    # use hard-coded date/time when it was on line for this test
+    # TODO remove these when the station is back on-line
+    sdt="2023-03-21 15:00:00"
+    edt="2023-03-21 15:15:00"  
+
+    # test with hard-coded time
+    readings = test_station.get_readings(start_datetime_str=sdt,end_datetime_str=edt)
+
+    # optional, log outputs for debug
+    # use pytest -s to see this output
+    logging.debug('{}\n'.format(test_station.current_response))
+        
+    assert test_station.current_response is not None
+    assert type(readings) is list
+    assert len(readings) >= 2
+    assert 'station_type' in readings[0].keys()
+    assert readings[0]['station_type'] == 'LOCOMOS'
+
+    for i in range(1,len(readings)):
+        transformed_reading = test_station.transform(data=readings[i])
+        assert len(transformed_reading.readings) > 0
+        for value in transformed_reading.readings:
+            assert isinstance(value.station_id, str)
+            assert isinstance(value.data_datetime, datetime)
+            assert isinstance(value.atemp, float)
+            assert isinstance(value.pcpn, float)
+            assert isinstance(value.relh, float)
+
+            logging.debug('\n{}: {}, {}, {}, {}, {}\n'.format(value.station_id,value.request_datetime,value.data_datetime,value.atemp,value.pcpn,value.relh))
