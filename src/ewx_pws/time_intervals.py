5e9d08c63bf24023511023527362bd2850d6728d
Pat Bills <billspat@msu.edu>
2023-06-27 17:03:46 -0400

    new classes for utc and intervals

--- src/ewx_pws/time_intervals.py
+++ src/ewx_pws/time_intervals.py
@@ -1,30 +1,103 @@
 """utils for editing time stamps"""
 
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
+from pytz import timezone, UTC
+timezone.utc = UTC
+from zoneinfo import ZoneInfo
+from pydantic import BaseModel, ValidationError, validator, root_validator
 
 
-def fifteen_minute_mark(dtm=datetime.now(timezone.utc)):
-    """return the nearest previous 15 minute mark.  e.g. 10:49 -> 10:45 """
+def is_tz_aware(dt:datetime)->bool:
+    """ based on documentation, test if a datetime is timezone aware (T) or naive (F)
+    see https://docs.python.org/3/library/datetime.html#determining-if-an-object-is-aware-or-naive
+    input dt a datetime object
+    returns True if is aware (not naive) False if not tz aware (naive)"""
+    if dt.tzinfo is not None:
+        if dt.tzinfo.utcoffset(dt) is not None:
+            return True
+    return False
+  
+
+class DatetimeUTC(BaseModel):
+    """singleton type to validate datetime has a timezone and convert to UTC if so. 
+    this was written for DatetimeInterval but no longer used for that. can be used to validate datetime value"""
+    datetime: datetime # Needs to be UTC
+    # @validator('value')
+    # def check_datetime_utc(cls, d):
+    #     assert d.tzinfo == UTC
+    #     return d
+
+    @root_validator(allow_reuse=True)
+    def must_be_tz_aware(cls, values):
+        dt = values.get('datetime')
+        if dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None:
+            values['datetime'] = dt.astimezone(timezone.utc)                
+        else:
+            raise ValueError('datetime must have a timezone')
+        return(values)
+    
+    # convenience functions
+    def strftime(self, *args, **kwargs):
+        self.datetime.strftime(args,kwargs)
+
+    def astimezone(self, *args, **kwargs):
+         self.datetime.astimezone(args, kwargs)
+
+
+class DatetimeInterval(BaseModel):
+    """ Type to hold a start and end time that have timezones.  Timezones will be converted to UTC.   Note it's preferred to use UTC 
+    datetimes from the beginning as local times that occur durng the DST transition will not be known for sure and will most
+    likely be incorrect"""
+    start: DatetimeUTC
+    end: DatetimeUTC
+    #
+    class Config:
+        allow_reuse=True
+    
+    # don't need this validator if using utc-enforced types     
+    # @validator('start','end',allow_reuse=True)
+    # def dt_must_have_timezone(cls, dt):
+    #     """ensure the date times provided have timezones, and convert those to UTC"""
+    #     if dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None:
+    #         return(dt.astimezone(timezone.utc))
+    #     else:
+    #         raise ValueError('datetime must have a timezone')
+    #
+    @root_validator(allow_reuse=True,pre=True)
+    def validate_utc_interval(cls, values):
+        """ensure that start is before end"""
+        start_dt = values.get('start')
+        end_dt = values.get('end')
+        #
+        if not start_dt.datetime <= end_dt.datetime:
+            raise ValueError('end date-time must come after start date-time')
+        return(values)
+
+
+def fifteen_minute_mark(dtm:datetime=datetime.now(timezone.utc))->datetime:
+    """return the nearest previous 15 minute mark.  e.g. 10:49 -> 10:45, preserves timezone if any. 
+    parameter dtm = optional datetime, default is 'now' using utc timezone """
     dtm -= timedelta(minutes=dtm.minute % 15,
                      seconds=dtm.second,
                      microseconds=dtm.microsecond)
     return(dtm)
 
 
-def previous_fifteen_minute_period(dtm=datetime.now(timezone.utc)):
-    """ returns tuple of start/end times that is on the quarter hour and inclusive.   
+def previous_fifteen_minute_period(dtm:datetime=datetime.now(timezone.utc))->tuple[datetime, datetime]:
+    """ returns tuple of start/end times that is on the quarter hour and inclusive. 
     input datetime object with timezone , e.g. 03:10:15+00
     output: tuple of two datetime objects, e.g (02:45:00, 03:00:00)
     
     if called successively every 15 minutes, times will overlap , e.g. 
     (02:45:00, 03:00:00), (03:00:00, 3:15:00),(3:15:00, 03:30:00), etc
+    
     """
     end_datetime = fifteen_minute_mark(dtm)
     start_datetime = end_datetime - timedelta(minutes=15)
     return((start_datetime, end_datetime))
 
 
-def previous_fourteen_minute_period( dtm = datetime.now(timezone.utc) ):
+def previous_fourteen_minute_period( dtm:datetime = datetime.now(timezone.utc) )->tuple[datetime, datetime]:
     """ returns tuple of start/end times that is on the quarter hour and not inclusive.   
     input datetime object with timezone , e.g. 03:10:15+00
     output: tuple of two datetime objects, e.g (02:46:00, 03:00:00)
@@ -34,4 +107,58 @@ def previous_fourteen_minute_period( dtm = datetime.now(timezone.utc) ):
     """
     end_datetime = fifteen_minute_mark(dtm)
     start_datetime =  end_datetime - timedelta(minutes=14)
-    return( (start_datetime, end_datetime))
+    return( (start_datetime, end_datetime) )
+
+
+def previous_fifteen_minute_interval(dtm:datetime=datetime.now(timezone.utc))->DatetimeInterval:
+    """ returns  that is on the quarter hour and inclusive. 
+    input datetime object with timezone , e.g. 03:10:15+00
+    output: tuple of two datetime objects, e.g (02:45:00, 03:00:00)
+    
+    if called successively every 15 minutes, times will overlap , e.g. 
+    (02:45:00, 03:00:00), (03:00:00, 3:15:00),(3:15:00, 03:30:00), etc
+    
+    """
+        
+    end_datetime = fifteen_minute_mark(dtm)  # must be utc
+    start_datetime = end_datetime - timedelta(minutes=15)
+    try:
+        dti = DatetimeInterval(start = start_datetime, end = end_datetime )
+    except ValueError as e:
+        raise(ValueError)
+        
+    return(dti)
+
+def previous_interval(dtm:datetime=datetime.now(timezone.utc), delta_mins:int=15)->DatetimeInterval:
+    """ returns  that is on the quarter hour and inclusive. 
+    input datetime object with timezone , e.g. 03:10:15+00
+    output: tuple of two datetime objects, e.g (02:45:00, 03:00:00)
+    
+    if called successively every 15 minutes, times will overlap , e.g. 
+    (02:45:00, 03:00:00), (03:00:00, 3:15:00),(3:15:00, 03:30:00), etc
+
+    set arbitrary delta (15 minutes, 14 minutes, 30 minutes, etc)
+    
+    """
+    # starter time
+    dtm_utc = DatetimeUTC(dtm)
+
+    # quarter hour prior to starter (11:55 ->  11:45, etc)
+    end_datetime_utc = DatetimeUTC(fifteen_minute_mark(dtm_utc.datetime))
+    # time previous to that for delta
+    start_datetime_utc = DatetimeUTC(end_datetime - timedelta(minutes=delta_mins))
+    
+    try:
+        dti = DatetimeInterval(start = start_datetime_utc, end = end_datetime_utc )
+    except ValueError as e:
+        raise(ValueError)
+        
+    return(dti)
+
+def previous_fourteen_minute_interval(dtm:datetime=datetime.now(timezone.utc))->DatetimeInterval:
+    """ convenience method for using previous interval above for 14 intervals, 
+    which are non-overlapping ranges of an hour
+    00:00 - 00:14, 00:15 - 00:29, 00:30 - 00:44, 00:45 - 00:59
+    """
+    dti = previous_interval(dtm, delta_mins=14)
+    return(dti)
--- tests/test_timeintervals.py
+++ tests/test_timeintervals.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
 
-import pytest, datetime
+import pytest
+from datetime import datetime, timedelta, timezone
+import pytz
+from pydantic import ValidationError
 
 from ewx_pws import time_intervals
-
+from ewx_pws.time_intervals import fifteen_minute_mark,previous_fifteen_minute_period, previous_fourteen_minute_period, \
+                                    DatetimeUTC, \
+                                    DatetimeInterval, previous_fourteen_minute_interval,previous_fifteen_minute_interval
 
 @pytest.fixture
 def test_timestamp():
     """return a random timestamp, but just now for now"""
-    return datetime.datetime.now(datetime.timezone.utc)
+    return datetime.now(timezone.utc)
 
+@pytest.fixture()
+def just_past_two():
+    return(datetime(datetime.now().year,1,1,hour=2, minute=1, second=0))
 
 def test_15minutemark(test_timestamp):
     """test that we get something always on 15 minutes"""
 
     # no args
-    dtm = time_intervals.fifteen_minute_mark()
+    dtm = fifteen_minute_mark()
     assert dtm.minute % 15 == 0
 
     # with args
-    dtm = time_intervals.fifteen_minute_mark(test_timestamp)
+    dtm = fifteen_minute_mark(test_timestamp)
     assert dtm.minute % 15 == 0
 
-    just_past_two = datetime.datetime(2022,1,1,hour=2, minute=1, second=0)
-    two = datetime.datetime(2022,1,1,hour=2, minute=0, second=0)
-    assert time_intervals.fifteen_minute_mark(just_past_two) == two
+    just_past_two = datetime(2022,1,1,hour=2, minute=1, second=0)
+    two = datetime(2022,1,1,hour=2, minute=0, second=0)
+    assert fifteen_minute_mark(just_past_two) == two
 
-    time_with_seconds  = datetime.datetime(2022,1,1,hour=2, minute=1, second=10)
-    assert time_intervals.fifteen_minute_mark(time_with_seconds) == two
+    time_with_seconds  = datetime(2022,1,1,hour=2, minute=1, second=10)
+    assert fifteen_minute_mark(time_with_seconds) == two
     
 
 
 def test_previous_fifteen_minute_period():
 
     
-    pfmp = time_intervals.previous_fifteen_minute_period()
+    pfmp = previous_fifteen_minute_period()
     assert len(pfmp) == 2
     assert pfmp[1] > pfmp[0]
     assert pfmp[0].minute % 15 == 0
@@ -45,21 +53,38 @@ def test_previous_fifteen_minute_period():
     if end_minute == 0: end_minute = 60
     assert  abs(end_minute - start_minute) == 15
 
-    now = datetime.datetime.now(datetime.timezone.utc)
-    pfmp = time_intervals.previous_fifteen_minute_period(now)
+    now = datetime.utcnow()
+    pfmp = previous_fifteen_minute_period(now)
     assert pfmp[1] <= now
-    assert now - pfmp[0] > datetime.timedelta(minutes=15)
+    assert now - pfmp[0] > timedelta(minutes=15)
     
     #TODO test that it's ac
     
-    sample_dt = datetime.datetime(2022,1,1,hour=2, minute=10, second=0)
-    pfmp = time_intervals.previous_fifteen_minute_period(sample_dt)
-    assert pfmp[1] == datetime.datetime(2022,1,1,hour=2, minute=0, second=0)
-    assert pfmp[0] == datetime.datetime(2022,1,1,hour=1, minute=45, second=0)
+    sample_dt = datetime(2022,1,1,hour=2, minute=10, second=0)
+    pfmp = previous_fifteen_minute_period(sample_dt)
+    assert pfmp[1] == datetime(2022,1,1,hour=2, minute=0, second=0)
+    assert pfmp[0] == datetime(2022,1,1,hour=1, minute=45, second=0)
 
 def test_previous_fourteen_minute_period():
-
-    pfmp = time_intervals.previous_fourteen_minute_period()
+    pfmp = previous_fourteen_minute_period()
         # check that the interval is really 14 minutes
     assert len(pfmp) == 2
     assert (pfmp[1] - pfmp[0]).seconds == 14 * 60
+
+def test_datetime_utc(just_past_two):
+    nowish = datetime.now(timezone.utc)
+    dtu = DatetimeUTC(datetime = nowish)
+    assert isinstance(dtu.datetime, datetime)
+    assert dtu.datetime.tzinfo == pytz.UTC
+
+    # fixture should not have a timezone, let's check
+    assert just_past_two.tzinfo is None
+    with pytest.raises(ValidationError):
+        DatetimeUTC(datetime = just_past_two)
+
+    # add a tz and check again    
+    dt_with_tz = pytz.timezone('US/Eastern').localize(just_past_two)
+    dtu = DatetimeUTC(datetime = dt_with_tz)
+    assert isinstance(dtu.datetime, datetime)
+    assert dtu.datetime.tzinfo == pytz.UTC
+
