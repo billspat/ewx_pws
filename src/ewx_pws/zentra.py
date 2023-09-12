3a7b5fdb32aaecd1108dc44f5de8848e72684baa
Pat Bills <billspat@msu.edu>
2023-09-12 14:36:45 -0400

    fix zentra transform #59 WIP

--- src/ewx_pws/zentra.py
+++ src/ewx_pws/zentra.py
@@ -27,7 +27,7 @@ class ZentraStation(WeatherStation):
         return(cls(station_config))
 
     
-    def __init__(self,config:ZentraConfig, max_retries : int = 0):
+    def __init__(self,config:ZentraConfig, max_retries : int = 2):
         self._max_retries : int = max_retries
         super().__init__(config)  
 
@@ -77,7 +77,7 @@ class ZentraStation(WeatherStation):
 
         return(response)
 
-    def _transform(self, response_data):
+    def _transform(self, response_data)->list:
         """
         Transforms response text from Zentra API into a standardized format 
         params:
@@ -101,29 +101,72 @@ class ZentraStation(WeatherStation):
         
         readings = []
         # Build a ZentraReading object for each and put it into the readings_list
-        for reading in response_data['data']['Air Temperature'][0]['readings']:
-            
-            # timestamp to use as an index to find associated sensor values
-            timestamp = reading['timestamp_utc']
+        # the readings are sensor-wise, not timestamp-wise, so we need to collect all the timestamps from 
+        # the first reading, then build our list sensor wise. 
 
-            # dict to hold the sensor values
-            timestamped_reading = {}
-            timestamped_reading['data_datetime'] = datetime.fromtimestamp(reading['timestamp_utc']).astimezone(timezone.utc)
-            timestamped_reading['atemp'] = reading['value']
+        # to get the timestamps, previously relied on hard-coded key to get one element
+        # reading in response_data['data']['Air Temperature'][0]['readings']
 
-            for reading2 in response_data['data']['Precipitation'][0]['readings']:
-                # this appears to be able to read multiple values, overwriting until the last
-                # rewwrite to extract without looping
-                if reading2['timestamp_utc'] == timestamp:
-                    timestamped_reading['pcpn'] = reading2['value']
-            for reading2 in response_data['data']['Relative Humidity'][0]['readings']:
-                # this appears to be able to read multiple values, overwriting until the last
-                if reading2['timestamp_utc'] == timestamp:
-                    timestamped_reading['relh'] = reading2['value']
+        # use the first sensor in data to get the list of timestamps.  
+        # data is keys on sensors
+
+    
+        # hard coded sensor transform names.  Update this to add more types of sensors.  assumes there is no transform of these values needed
+        sensor_transforms = {'Air Temperature':'atemp', 'Precipitation':'pcpn','Relative Humidity':'relh'}
+        station_sensors = response_data['data'].keys()
+
+        # list_of_readings_in_data = response_data['data'][station_sensors[0]][0]['readings']
+        # a reading is a sensor-per-timestamp
+
+        # a dict of dict keyed on timestamp
+        readings_by_timestamp = {}
+
+        for sensor in response_data['data']: # response_data['data'].keys()
             
-            readings.append(timestamped_reading)
+
+            if sensor in sensor_transforms: # only include those sensors that we have transforms for
+                # loop through the readings of this sensor, build up timestamp-keyed dict of dict
+                for zentra_reading in response_data['data'][sensor][0]['readings']:
+
+                    timestamp = zentra_reading['timestamp_utc']
+                    # if we haven't seen this timestamp before, create a new entry in our readings dict
+                    if timestamp not in readings_by_timestamp:
+                        # add the datetime which would be the same for all readings with this time stamp
+                        reading_datetime = datetime.fromtimestamp(timestamp).astimezone(timezone.utc)
+                        readings_by_timestamp[timestamp] = {'data_datetime': reading_datetime}
+                    
+                    # add this sensor value to our ewx reading dict
+                    ewx_field_name = sensor_transforms[sensor]
+                    readings_by_timestamp[timestamp][ewx_field_name] = zentra_reading['value']
+
+
+
+        # for reading in list_of_readings_in_data:
+        #     for sensor in sensors: 
+
+        #     response_data['data']['Air Temperature'][0]['readings']
+        #     # timestamp to use as an index to find associated sensor values
+        #     timestamp = reading['timestamp_utc']
+
+        #     # dict to hold the sensor values
+        #     timestamped_reading = {}
+        #     timestamped_reading['data_datetime'] = datetime.fromtimestamp(reading['timestamp_utc']).astimezone(timezone.utc)
+        #     timestamped_reading['atemp'] = reading['value']
+
+        #     for reading2 in response_data['data']['Precipitation'][0]['readings']:
+        #         # this appears to be able to read multiple values, overwriting until the last
+        #         # rewwrite to extract without looping
+        #         if reading2['timestamp_utc'] == timestamp:
+        #             timestamped_reading['pcpn'] = reading2['value']
+        #     for reading2 in response_data['data']['Relative Humidity'][0]['readings']:
+        #         # this appears to be able to read multiple values, overwriting until the last
+        #         if reading2['timestamp_utc'] == timestamp:
+        #             timestamped_reading['relh'] = reading2['value']
             
-        return readings
+        #     readings.append(timestamped_reading)
+
+        # return just the list ()
+        return readings_by_timestamp.values()
     
     def _handle_error(self):
         """ place holder to remind that we need to add err handling to each class"""
